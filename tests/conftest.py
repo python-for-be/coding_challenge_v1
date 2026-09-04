30185246b13f937e40e648f2eb663f63c5fba70c
Michael Brown <python_for_be@tutamail.com>
2026-09-04 15:59:05 +0100

    Configure Ruff for linting and formatting with Pyflakes and pycodestyle rules. Ran formatting and linting

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,5 @@ from src.main import app
 
 @pytest.fixture
 async def async_test_client():
-    async with AsyncClient(
-            transport=ASGITransport(app=app), base_url="http://test"
-    ) as client:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as client:
         yield client
