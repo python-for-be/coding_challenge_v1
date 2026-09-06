9072b789fb3338a132d9f01c37ba982418ecb039
Michael Brown <python_for_be@tutamail.com>
2026-09-06 11:23:18 +0100

    Add `create_user` method to `UserService` for creating users with addresses

--- src/services/users.py
+++ src/services/users.py
@@ -1,7 +1,7 @@
 from typing import List, Any
 
 from src.repositories.users import UserRepository
-from src.schemas.users import UsersResponse
+from src.schemas.users import UsersResponse, UserCreateSchema
 
 
 class UserService:
@@ -14,3 +14,14 @@ class UserService:
         """Retrieve all users."""
         users = await self.repository.list_users()
         return [UsersResponse.model_validate(user).model_dump() for user in users]
+
+    async def create_user(self, user_in: UserCreateSchema) -> dict[str, Any]:
+        """Create a new user.
+
+        Args:
+            user_in (UserCreateSchema): User object to create.
+        """
+        user_data = user_in.model_dump()
+        address_data = user_data.pop("address")
+        user = await self.repository.create_user(user_data, address_data)
+        return UsersResponse.model_validate(user).model_dump()
